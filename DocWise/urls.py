5cff90da1249173ef41ca3626e8b208c72a4bf67
Shlok Arora <shlokarora2709@gmail.com>
2024-07-11 20:06:58 +0530

    chore: Add missing import statement and static URL configuration

--- DocWise/urls.py
+++ DocWise/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Login.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,5 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('', home_view, name='home'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Login/views.py
+++ Login/views.py
@@ -21,9 +21,12 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 return redirect('home')
+        else:
+            error="Invalid username or password"
     else:
         form = CustomAuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+        error=None
+    return render(request, 'login.html', {'form': form,'error': error})
 
 def logout_view(request):
     logout(request)
