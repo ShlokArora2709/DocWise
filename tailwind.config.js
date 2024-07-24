4bd19256807aa358b998a7e1270a4548fd299fed
Shlok Arora <shlokarora2709@gmail.com>
2024-07-24 18:34:42 +0530

    chore: Update login page styles and form layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    './templates/**/*.{html,js}',  // Adjust paths based on your project structure
+    './static/**/*.{html,js}',
+    './Login/static/**/*.{html,js}', 
+
+  ],
   theme: {
     extend: {
       colors: {
         'custom-fuchsia': '#4a053b',
-        'custom-blue': '#027ea9',
+        'custom-blue': '#312e81',
         'custom-yellow': '#e9df0e',
         'custom-teal': '#31e4b3',
         'custom-pink': '#e72da2',
